d083ffe5ae9a3a3c6b5f0735cc621cd8993b8db6
Jordan Wathen <jordan.r.wathen@accenture.com>
2021-02-07 13:58:01 +0000

    Added methods to Lesson class
    added methods to lesson to enable getID() etc

--- src/main/java/org/github/jrds/server/Lesson.java
+++ src/main/java/org/github/jrds/server/Lesson.java
@@ -8,6 +8,35 @@ public class Lesson {
     private String educator; // TODO - (not yet) user eventually ?, user & userStore classes 
     private Set<String> learners; // TODO - (not yet) user not string 
 
-    //TODO constructor
+    public Lesson(String id, String educator, Set<String> learners) {
+        this.id = id;
+        this.educator = educator;
+        this.learners = learners;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEducator() {
+        return educator;
+    }
+
+    public Set<String> getLearners() {
+        return learners;
+    }
+
+    public void addLearner(String learner) { // TODO this will eventually by passing in user, and user.getName user.getID etc will be used
+        learners.add(learner);
+    }
+
+    public boolean checkForALearner(String learner) {
+        if (learners.contains(learner)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 }
