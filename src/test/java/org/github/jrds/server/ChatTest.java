2e517d3f2a061a2193a9af50ef15c2382d464573
Jordan Wathen <jordan.wathen5@gmail.com>
2021-03-12 12:19:33 +0000

    Updated chatTest to match the changes now 3/4 chat tests passing.
    31/35 tests passing.

--- src/test/java/org/github/jrds/server/ChatTest.java
+++ src/test/java/org/github/jrds/server/ChatTest.java
@@ -30,16 +30,13 @@ public class ChatTest extends ApplicationTest
         Assert.assertTrue(sentMessageResponse.isSuccess());
 
         Message received = c1.getMessageReceived();
-        Assert.assertTrue(received instanceof ChatMessage);
-        ChatMessage receivedChat = (ChatMessage) received;
 
-        Assert.assertEquals(c2.getId(), receivedChat.getFrom());
-        Assert.assertEquals(c1.getId(), receivedChat.getTo());
-        Assert.assertEquals(msg, receivedChat.getText());
+        Assert.assertTrue(received instanceof ChatMessage);
+        Assert.assertTrue(messageContentAsExpected(c2.getId(), c1.getId(), msg, (ChatMessage) received));
     }
 
     @Test
-    public void educatorReceivesMessagesFrom2LearnersSimultaneously()
+    public void educatorReceivesMessagesFrom2LearnersSimultaneously() throws InterruptedException, ExecutionException, TimeoutException
     {
         String msg1 = "Learner 1 - Test message";
         String msg2 = "Learner 2 - Test message";
@@ -48,29 +45,46 @@ public class ChatTest extends ApplicationTest
         TestClient c2 = connect(l1Id, l1Name, lesson1);
         TestClient c3 = connect(l2Id, l2Name, lesson1);
 
-        c2.sendChatMessage(msg1, c1.getId());
-        c3.sendChatMessage(msg2, c1.getId());
+        Future<Response> sentMessageFuture1 = c2.sendChatMessage(msg1, c1.getId());
+        Response sentMessageResponse1 = sentMessageFuture1.get(10, TimeUnit.SECONDS);
+
+        Future<Response> sentMessageFuture2 = c3.sendChatMessage(msg2, c1.getId());
+        Response sentMessageResponse2 = sentMessageFuture2.get(10, TimeUnit.SECONDS);
+
+        Assert.assertTrue(sentMessageResponse1.isSuccess());
+        Assert.assertTrue(sentMessageResponse2.isSuccess());
 
         List<Message> messagesReceived = Arrays.asList(c1.getMessageReceived(), c1.getMessageReceived());
-        Assert.assertTrue(messagesReceived.contains(new ChatMessage(l1Id, eduId, msg1)));
-        Assert.assertTrue(messagesReceived.contains(new ChatMessage(l2Id, eduId, msg2)));
+
+        Assert.assertTrue(messagesReceived.get(0) instanceof ChatMessage);
+        Assert.assertTrue(messageContentAsExpected(c2.getId(), c1.getId(), msg1, (ChatMessage) messagesReceived.get(0)));
+
+        Assert.assertTrue(messagesReceived.get(1) instanceof ChatMessage);
+        Assert.assertTrue(messageContentAsExpected(c3.getId(), c1.getId(), msg2, (ChatMessage) messagesReceived.get(1)));
     }
 
     @Test
-    public void educatorReceivesMessageAndResponds()
+    public void educatorReceivesMessageAndResponds() throws InterruptedException, ExecutionException, TimeoutException
     {
         String response = "Educator response - Test message";
 
         TestClient c1 = connect(eduId, eduName, lesson1);
         TestClient c2 = connect(l1Id, l1Name, lesson1);
 
-        c2.sendChatMessage(msg, c1.getId());
-        Message received = c1.getMessageReceived();
-        Assert.assertEquals(new ChatMessage(l1Id, eduId, msg), received);
+        Future<Response> sentMessageFuture1 = c2.sendChatMessage(msg, c1.getId());
+        Response sentMessageResponse1 = sentMessageFuture1.get(10, TimeUnit.SECONDS);
 
-        c1.sendChatMessage(response, c2.getId());
-        Message responseReceived = c2.getMessageReceived();
-        Assert.assertEquals(new ChatMessage(eduId, l1Id, response), responseReceived);
+        Future<Response> sentMessageFuture2 = c1.sendChatMessage(response, c2.getId());
+        Response sentMessageResponse2 = sentMessageFuture2.get(10, TimeUnit.SECONDS);
+
+        Message educatorReceived = c1.getMessageReceived();
+        Assert.assertTrue(educatorReceived instanceof ChatMessage);
+        Assert.assertTrue(messageContentAsExpected(c2.getId(), c1.getId(), msg, (ChatMessage) educatorReceived ));
+
+        Message learnerReceived = c2.getMessageReceived();
+        System.out.println(learnerReceived);
+        Assert.assertTrue(learnerReceived instanceof ChatMessage);
+        Assert.assertTrue(messageContentAsExpected(c1.getId(), c2.getId(), response, (ChatMessage) learnerReceived ));
     }
 
     @Test
@@ -97,4 +111,20 @@ public class ChatTest extends ApplicationTest
         Assert.assertTrue(messagesReceived.contains(new ChatMessage(eduId, l1Id, response2)));
         Assert.assertTrue(messagesReceived.contains(new ChatMessage(eduId, l2Id, response1)));
     }
+
+    private boolean messageContentAsExpected(String from, String to, String text, ChatMessage cM)
+    {
+        try
+        {
+            Assert.assertEquals(from, cM.getFrom());
+            Assert.assertEquals(to, cM.getTo());
+            Assert.assertEquals(text, cM.getText());
+            return true;
+        }
+        catch (AssertionError e)
+        {
+            return false;
+        }
+    }
+
 }
