afd93ad5f961254e5bf5dda5e721cc0b179beb6b
Jordan Wathen <jordan.wathen5@gmail.com>
2021-04-10 10:28:06 +0100

    Addtional learners added

--- src/main/java/org/github/jrds/server/persistence/UsersStore.java
+++ src/main/java/org/github/jrds/server/persistence/UsersStore.java
@@ -21,7 +21,10 @@ public class UsersStore
         // These match to the strings defined in ApplicationTest.java
         storeUser(new User("u1900", "Jordan"));
         storeUser(new User("e0001", "Educator Rebecca"));
-        storeUser(new User("u1901", "Savanna"));
+        storeUser(new User("u1901", "Ambre"));
+        storeUser(new User("u1902", "Aaron"));
+        storeUser(new User("u1903", "Anthony"));
+        storeUser(new User("u1904", "Juliette"));
         storeUser(new User("u9999", "Jack"));
     }
 
