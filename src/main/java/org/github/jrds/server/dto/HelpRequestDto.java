542dabc9f70128d431b8d2de7f849fceb6037e4a
Jordan Wathen <jordan.wathen5@gmail.com>
2021-04-13 12:03:56 +0100

    Added name to HelpRequestDto

--- src/main/java/org/github/jrds/server/dto/HelpRequestDto.java
+++ src/main/java/org/github/jrds/server/dto/HelpRequestDto.java
@@ -10,28 +10,36 @@ import java.time.Instant;
 public class HelpRequestDto
 {
     private final String learnerId;
+    private final String learnerName;
     private final Instant timeReceived;
     private final String status;
 
     @JsonCreator
     public HelpRequestDto(
             @JsonProperty("learnerId") String learnerId,
+            @JsonProperty("learnerName") String learnerName,
             @JsonProperty("timeReceived") Instant timeReceived,
             @JsonProperty("status") String status)
     {
         this.learnerId = learnerId;
         this.timeReceived = timeReceived;
         this.status = status;
+        this.learnerName = learnerName;
     }
 
     public HelpRequestDto(HelpRequest helpRequest)
     {
-        this(helpRequest.getLearner().getId(), helpRequest.getTimeReceived(), helpRequest.getStatus().name());
+        this(helpRequest.getLearner().getId(), Main.defaultInstance.usersStore.getUser(helpRequest.getLearner().getId()).getName(),helpRequest.getTimeReceived(), helpRequest.getStatus().name());
     }
 
     public String getLearnerId()
     {
         return learnerId;
+    }
+
+    public String getLearnerName()
+    {
+        return learnerName;
     }
 
     public Instant getTimeReceived()
